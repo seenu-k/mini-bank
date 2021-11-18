bd8b07fd8f771a083db2e8d57bd5abf587e6c781
Seenu K <iitm.seenuk@gmail.com>
2021-11-18 17:35:35 +0530

    Add utility functions to convery balance strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mini-bank';
+
+  title = 'Mini Bank - Coding Design Assessment';
+  balance = 0;
+  operation = new FormControl('balance');
+  amount = new FormControl('');
+
+  balanceStringToBalance(balanceString: string) {
+    balanceString = balanceString.trim();
+    let [D, C] = [0, 0]
+    if(balanceString.indexOf(" ") >= 0) {
+      [D, C] = balanceString.split(" ").map(val => parseInt(val.slice(0, -1)));
+    }
+    else {
+      let val = parseInt(balanceString.slice(0, -1))
+      if(balanceString.substr(-1) === 'D') {
+        D = val;
+      }
+      else {
+        C = val;
+      }
+    }
+    return D+(C/100); // Think about negative D
+  }
+
+  balanceToBalanceString(balance: number) {
+    let D = Math.floor(balance);
+    let C = (balance*100)%100;
+    if(D===0) {
+      return C+"C";
+    }
+    if(C==0) {
+      return D+"D";
+    }
+    return D+"D "+C+"C";
+  }
+
 }
